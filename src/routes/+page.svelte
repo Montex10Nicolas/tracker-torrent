40d5d99662bc7eec25fff395313eadc1bae3ad72
Nicolas10Montex <montexdev@protonmail.com>
2026-01-16 23:06:53 +0100

    Page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+  import CreateRecord from "$lib/components/Records/CreateRecord.svelte";
+  import RecordList from "$lib/components/Records/RecordList.svelte";
+</script>
+
+<main>
+  <CreateRecord />
+  <RecordList />
+</main>
